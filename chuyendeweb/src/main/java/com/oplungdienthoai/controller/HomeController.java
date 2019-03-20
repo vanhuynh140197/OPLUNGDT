8a18508932b5b0f07a2c726eac413a1ba6ea9521
dieunguyen78 <nguyenthithuydieu1997@gmail.com>
2019-03-20 12:43:25 +0700

    HomeController.java

---
+++ chuyendeweb/src/main/java/com/oplungdienthoai/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.oplungdienthoai.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value ="/oplungdienthoai")
+public class HomeController {
+
+	@RequestMapping(value ="/home")
+	public String index(){
+		return "/ui/index";
+	}
+}
